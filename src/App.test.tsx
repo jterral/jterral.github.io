324aaefaf28fb35ebe6bd4d4153902e3a2d19bab
Jonathan T <jonathan.terral@outlook.com>
2022-07-24 12:26:22 +0200

    feat(analytics): add counter dev (#126)
    * chore: use yarn and udate packages
    * chore: update gh pages
    * feat: add counter dev
    * chore: check packages on friday
    * feat: add og meta tags

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,39 +3,36 @@ import { MemoryRouter } from "react-router-dom";
 import App from "./App";
 
 describe("<App />", () => {
-    test("should display LandingPage for default route", () => {
-        // Arrange
-        const app = (
-            <MemoryRouter>
-                <App />
-            </MemoryRouter>
-        );
+  test("should display LandingPage for default route", () => {
+    // Arrange
+    const app = (
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
 
-        // Act
-        render(app);
+    // Act
+    render(app);
 
-        // Assert
-        expect(screen.getByTestId("landing")).toBeInTheDocument();
-    });
+    // Assert
+    expect(screen.getByTestId("landing")).toBeInTheDocument();
+  });
 
-    test("should render CityParkingsPage for cityparkings routes", () => {
-        // Arrange
-        const app = (
-            <MemoryRouter
-                initialEntries={[
-                    "/cityparkings",
-                    "/cityparkings/privacy-policy",
-                ]}
-            >
-                <App />
-            </MemoryRouter>
-        );
+  test("should render CityParkingsPage for cityparkings routes", () => {
+    // Arrange
+    const app = (
+      <MemoryRouter
+        initialEntries={["/cityparkings", "/cityparkings/privacy-policy"]}
+      >
+        <App />
+      </MemoryRouter>
+    );
 
-        // Act
-        render(app);
+    // Act
+    render(app);
 
-        // Assert
-        expect(screen.getByTestId("cityparkings-appbar")).toBeInTheDocument();
-        expect(screen.getByTestId("cityparkings-body")).toBeInTheDocument();
-    });
+    // Assert
+    expect(screen.getByTestId("cityparkings-appbar")).toBeInTheDocument();
+    expect(screen.getByTestId("cityparkings-body")).toBeInTheDocument();
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import CityParkingsPage from "./pages/cityparkings/CityParkingsPage";
 import LandingPage from "./pages/landing/LandingPage";
 
 export default function App() {
-    return (
-        <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="cityparkings/*" element={<CityParkingsPage />} />
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/apps/cityparkings" element={<CityParkingsPage />} />
+      <Route
+        path="cityparkings/*"
+        element={<Navigate replace to="/apps/cityparkings" />}
+      />
+      <Route path="*" element={<Navigate replace to="/" />} />
+    </Routes>
+  );
 }
