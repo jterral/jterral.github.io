bd7ecc14035a66adfca23cc1e27a7792991a5806
Jonathan <jonathan.terral@outlook.com>
2025-12-19 18:15:06 +0000

    feat: rename work into apps

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,9 +2,9 @@ import { defineCollection, z } from "astro:content";
 import { glob } from "astro/loaders";
 
 export const collections = {
-  work: defineCollection({
-    // Load Markdown files in the src/content/work directory.
-    loader: glob({ base: "./src/content/work", pattern: "**/*.md" }),
+  apps: defineCollection({
+    // Load Markdown files in the src/content/apps directory.
+    loader: glob({ base: "./src/content/apps", pattern: "**/*.md" }),
     schema: z.object({
       title: z.string(),
       description: z.string(),
